00f36de9de9c0237877408955c772712760dbe76
Tsechelidis Michail <tsechelidis.michail@gmail.com>
2023-06-01 12:55:40 +0300

    Create domain_imp

---
+++ domain_imp/src/main/java/domain_imp/MovieImp.java
@@ -0,0 +1,14 @@
+package domain_imp;
+
+import domain.Movie;
+import lombok.Getter;
+
+@Getter
+public class MovieImp implements Movie {
+
+    public MovieImp(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
