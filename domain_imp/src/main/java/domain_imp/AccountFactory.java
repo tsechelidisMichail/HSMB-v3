034ae7105d803448761face151b8a5a655c722da
Tsechelidis Michail <tsechelidis.michail@gmail.com>
2023-06-03 10:11:36 +0300

    test commit

--- domain_imp/src/main/java/domain_imp/AccountFactory.java
+++ domain_imp/src/main/java/domain_imp/AccountFactory.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AccountFactory implements AbstractAccountFactory {
-//this is a test comment
+//this is a test comment2
     @Override
     public Account createAccount(int id, int balance) {
         return new AccountImp(id, balance);
