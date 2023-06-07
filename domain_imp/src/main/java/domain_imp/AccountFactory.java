2b69b50dc8b7c69210de63d45b0d669a9b8dddb6
Tsechelidis Michail <tsechelidis.michail@gmail.com>
2023-06-07 18:06:07 +0300

    test
    should update domain

--- domain_imp/src/main/java/domain_imp/AccountFactory.java
+++ domain_imp/src/main/java/domain_imp/AccountFactory.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AccountFactory implements AbstractAccountFactory {
-
     @Override
     public Account createAccount(int id, int balance) {
         return new AccountImp(id, balance);
